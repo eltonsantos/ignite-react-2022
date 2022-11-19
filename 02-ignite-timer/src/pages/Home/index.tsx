63c350e16dacd3b372abcc0e2f1c1f73ccb37dfb
Elton Santos <eltonaxl@hotmail.com>
2022-11-19 04:58:20 -0300

    02-ignite-timer

---
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -0,0 +1,46 @@
+import { Play } from 'phosphor-react'
+
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
